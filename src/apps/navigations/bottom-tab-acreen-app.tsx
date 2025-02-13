12407ac07787029db5ff466eb29291a8394c342e
GiaHuyIUH <tranhuyzaza@gmail.com>
2025-02-13 16:26:32 +0700

    config BottomTab

--- src/apps/navigations/bottom-tab-acreen-app.tsx
+++ src/apps/navigations/bottom-tab-acreen-app.tsx
@@ -1,12 +1,41 @@
-import { HomeScreen, UserScreen } from "@/apps/screens";
+import { HomeScreen, SearchScreen, MessageScreen, UserScreen } from "@/apps/screens";
 import { Tab } from "@/libs/navigation";
+import { Ionicons } from "@expo/vector-icons";
 
 export const BottomTabScreenApp = () => {
 	return (
-		<Tab.Navigator>
+		<Tab.Navigator
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ color, size }) => {
+					let iconName: any;
+					if (route.name === "HomeScreen") iconName = "home";
+					else if (route.name === "SearchScreen") iconName = "search";
+					else if (route.name === "MessageScreen") iconName = "chatbubble";
+					else if (route.name === "UserScreen") iconName = "person";
+					return (
+						<Ionicons
+							name={iconName}
+							size={size}
+							color={color}
+						/>
+					);
+				},
+				tabBarActiveTintColor: "#E67E22",
+				tabBarInactiveTintColor: "gray",
+				tabBarShowLabel: false,
+			})}
+		>
 			<Tab.Screen
 				name="HomeScreen"
 				component={HomeScreen}
+			/>
+			<Tab.Screen
+				name="SearchScreen"
+				component={SearchScreen}
+			/>
+			<Tab.Screen
+				name="MessageScreen"
+				component={MessageScreen}
 			/>
 			<Tab.Screen
 				name="UserScreen"
