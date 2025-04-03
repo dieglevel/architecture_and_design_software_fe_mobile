f5e6f561039f43e3e5d298256a6921baa8461222
hyuga127 <tranhuyzaza@gmail.com>
2025-04-03 16:25:57 +0700

    update: Bottom Tab with svg icon

--- src/apps/navigations/bottom-tab-acreen-app.tsx
+++ src/apps/navigations/bottom-tab-acreen-app.tsx
@@ -4,29 +4,70 @@ import HomeStack from "../stacks/home-stack";
 import SearchStack from "../stacks/search-stack";
 import MessageStack from "../stacks/message-stack";
 import UserStack from "../stacks/user-stack";
+import HomeIcon from "@/assets/svgs/home";
+import SearchIcon from "@/assets/svgs/search";
+import MessageIcon from "@/assets/svgs/message";
+import UserIcon from "@/assets/svgs/user";
+import { TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 
 export const BottomTabScreenApp = () => {
 	return (
 		<Tab.Navigator
 			screenOptions={({ route }) => ({
-				tabBarIcon: ({ color, size }) => {
-					let iconName: any;
-					if (route.name === "HomeStack") iconName = "home";
-					else if (route.name === "SearchStack") iconName = "search";
-					else if (route.name === "MessageStack") iconName = "chatbubble";
-					else if (route.name === "UserStack") iconName = "person";
-					return (
-						<Ionicons
-							name={iconName}
-							size={size}
-							color={color}
-						/>
-					);
+				tabBarIcon: ({ focused }) => {
+					let iconColor = focused ? "#fff" : "#461409";
+					let backgroundColor = focused ? "#f27052" : "#fff";
+					let size = 40;
+					if (route.name === "HomeStack")
+						return (
+							<HomeIcon
+								size={size}
+								color={iconColor}
+								backgroundColor={backgroundColor}
+							/>
+						);
+					else if (route.name === "SearchStack")
+						return (
+							<SearchIcon
+								size={size}
+								color={iconColor}
+								backgroundColor={backgroundColor}
+							/>
+						);
+					else if (route.name === "MessageStack")
+						return (
+							<MessageIcon
+								size={size}
+								color={iconColor}
+								backgroundColor={backgroundColor}
+							/>
+						);
+					else if (route.name === "UserStack")
+						return (
+							<UserIcon
+								size={size}
+								color={iconColor}
+								backgroundColor={backgroundColor}
+							/>
+						);
 				},
-				tabBarActiveTintColor: "#E67E22",
-				tabBarInactiveTintColor: "gray",
+				tabBarButton: (props: any) => (
+					<TouchableOpacity
+						{...props} // Chuyển tiếp tất cả các props
+						activeOpacity={0.7} // Chỉnh độ mờ khi nhấn
+					>
+						{/* Thay icon theo từng route */}
+						{props.children}
+					</TouchableOpacity>
+				),
+				tabBarIconStyle: {
+					width: "100%",
+					height: "100%",
+					margin: 0,
+				},
 				tabBarShowLabel: false,
 				headerShown: false,
+				tabBarHideOnKeyboard: true,
 			})}
 		>
 			<Tab.Screen
--- src/types/implement/user.ts
+++ src/types/implement/user.ts
@@ -1,7 +1,21 @@
+export enum Gender {
+	Male = 0,
+	Female = 1,
+}
+
+export enum UserRole {
+	USER = "USER",
+	ADMIN = "ADMIN",
+}
+
 export interface User {
+	userId?: string;
+	username?: string;
+	fullName?: string;
 	email?: string;
 	phone?: string;
-	username?: string;
-	fullname?: string;
-	userId?: string;
+	avatarUrl?: string;
+	birthday?: string | null;
+	gender?: Gender;
+	role?: UserRole;
 }
