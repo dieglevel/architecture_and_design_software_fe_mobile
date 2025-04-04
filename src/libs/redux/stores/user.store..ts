8d460846b5c5592e8986f828dce388328acb6539
hyuga127 <tranhuyzaza@gmail.com>
2025-04-04 23:04:58 +0700

    add: reset to reset redux

--- src/libs/redux/stores/user.store..ts
+++ src/libs/redux/stores/user.store..ts
@@ -2,24 +2,25 @@ import { User } from "@/types/implement";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface UserState {
-   data: User | null;
+	data: User | null;
 }
 
 const initialState: UserState = {
-   data: null,
+	data: null,
 };
 
 const userSlice = createSlice({
-   name: "user",
-   initialState,
-   reducers: {
-      setUser: (state, action: PayloadAction<User | null>) => {
-         state.data = action.payload;
-      },
-   },
-})
+	name: "user",
+	initialState,
+	reducers: {
+		setUser: (state, action: PayloadAction<User | null>) => {
+			state.data = action.payload;
+		},
+		reset: () => initialState,
+	},
+});
 
-export const { setUser } = userSlice.actions;
+export const { setUser, reset } = userSlice.actions;
 
 const userReducer = userSlice.reducer;
 export default userReducer;
